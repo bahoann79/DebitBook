ac2dc726d301c26f22fb20be9264dfac557b4a1a
bahoann79 <89914081+bahoann79@users.noreply.github.com>
2023-03-12 10:49:00 +0700

    update RegisterController
    control date user input from register screen

--- DebitBook/src/java/controller/user/RegisterController.java
+++ DebitBook/src/java/controller/user/RegisterController.java
@@ -9,7 +9,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import model.Account;
+import model.OTPRequest;
+import service.AccountService;
+import service.OTPService;
+import service.UserService;
+import util.Email;
 
 /**
  *
@@ -26,5 +31,38 @@ public class RegisterController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        boolean gender = request.getParameter("gender").equals("male");
+        String password = request.getParameter("password");
+        String phoneNumber = request.getParameter("phoneNumber");
+        String address = request.getParameter("address");
+        String defaultImg = gender ? "https://tse2.mm.bing.net/th?id=OIP.zL8PN3yVgzgZxy9ECCdXugHaHc&pid=Api&P=0" : "https://www.pngkit.com/png/full/115-1150342_user-avatar-icon-iconos-de-mujeres-a-color.png";
+        String img = (request.getParameter("img").length() == 0 || request.getParameter("img") == null) ? defaultImg : request.getParameter("img");
+
+        AccountService accountService = new AccountService();
+        boolean flag = accountService.get(name);
+
+        if (flag) {
+            String errorMessage = "Email already exists. Please try again !";
+            request.setAttribute("errorMessage", errorMessage);
+            request.getRequestDispatcher("/views/user/register.jsp").forward(request, response);
+        } else {
+            accountService.insert(email, password);
+            UserService userService = new UserService();
+            int userId = userService.insert(name, email, gender, phoneNumber, address, img);
+
+            int otpType = Integer.parseInt(request.getParameter("otpType"));
+            OTPService otpService = new OTPService();
+            String otpCode = otpService.createOTPRequest(userId, otpType);
+
+            OTPRequest otp = otpService.sendMail(email, otpCode);
+            
+            request.setAttribute("otp", otp);
+            request.getRequestDispatcher("views/user/verifyEmail.jsp").forward(request, response);
+
+        }
+
     }
 }
